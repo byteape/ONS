e4c8f476bc5cd3e2bed767700bcd7a01494dd0f0
William <gjhost@sina.com>
2016-08-04 14:35:47 +0800

    ONSV1.0

---
+++ del.php
@@ -0,0 +1,6 @@
+﻿<?php 
+require 'Library/common.php';
+error_reporting(E_ALL ^E_NOTICE);
+$id=$_GET['id'];
+$re=$M->table('rules')->where(array('id'=>$id))->delete();
+header("Location:manage.php");
